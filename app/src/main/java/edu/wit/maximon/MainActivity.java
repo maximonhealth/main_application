4c442fa3c05b76ed8aea5434cd273f66eb040bb8
zshel <sheltonz@wit.edu>
2018-11-14 21:06:35 -0500

    Finish fragment implementation

--- app/src/main/java/edu/wit/maximon/MainActivity.java
+++ app/src/main/java/edu/wit/maximon/MainActivity.java
@@ -1,13 +1,22 @@
 package edu.wit.maximon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import edu.wit.maximon.fragments.HomeFragment;
+import edu.wit.maximon.fragments.OnFragmentInteractionListener;
+import edu.wit.maximon.fragments.SettingsFragment;
+import edu.wit.maximon.fragments.TimelineFragment;
+
+public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
+
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -17,26 +26,42 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_timeline:
-
-                    return true;
+                    applyFragment(new TimelineFragment(), R.id.fragment);
+                    break;
                 case R.id.navigation_home:
-
-                    return true;
+                    applyFragment(new HomeFragment(), R.id.fragment);
+                    break;
                 case R.id.navigation_settings:
-
-                    return true;
+                    applyFragment(new SettingsFragment(), R.id.fragment);
+                    break;
             }
-            return false;
+            return true;
         }
     };
+
+    private void applyFragment(final Fragment fragment, final int id) {
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(id, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_home);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
