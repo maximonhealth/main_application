12b046faca42f76d5d5de5917e5ebc34bc2af0fa
shanmcgov <shanmcgov77@gmail.com>
2018-12-04 18:48:26 -0500

    v minor changes. 3 words to be exact

--- app/src/main/java/edu/wit/maximon/fragments/SettingsFragment.java
+++ app/src/main/java/edu/wit/maximon/fragments/SettingsFragment.java
@@ -2,18 +2,27 @@ package edu.wit.maximon.fragments;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.service.notification.NotificationListenerService;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.util.Log;
 
+import edu.wit.maximon.MainActivity;
 import edu.wit.maximon.R;
 
 
@@ -35,7 +44,7 @@ public class SettingsFragment extends CustomFragment {
         super(parentActivity);
     }
 
-    SharedPreferences mySettings = getParentActivity().getSharedPreferences("Settings", Context.MODE_PRIVATE);
+    SharedPreferences mySettings = getParentActivity().getSharedPreferences("default_prefs", Context.MODE_PRIVATE);
     SharedPreferences.Editor editor = mySettings.edit();
 
     int id = 1;
@@ -85,4 +94,15 @@ public class SettingsFragment extends CustomFragment {
         super.onDetach();
         mListener = null;
     }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked) {
+            Log.v("PB", "Notifications are currently ON");
+        }
+        else {
+            Log.v("PB", "Notifications are currently OFF");
+        }
+    }
 }
+
+
